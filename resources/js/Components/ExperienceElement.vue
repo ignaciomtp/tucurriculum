093d438346bd2b73194eeb524b20f68b2a406432
ignaciomtp <ignaciomtp@gmail.com>
2024-02-23 11:31:21 +0100

    Acordeón experiencias

--- resources/js/Components/ExperienceElement.vue
+++ resources/js/Components/ExperienceElement.vue
@@ -75,178 +75,180 @@ const deleteExp = () => {
 </script>
 
 <template>
-	<form @submit.prevent="submit">
-		<div class="flex">
-			<div class="flex-auto w-33 ">
-			   	<div class="p-2 m-2">
-	                <InputLabel for="title" value="Puesto" />
-
-	                <TextInput
-	                    id="title"
-	                    type="text"
-	                    class="mt-1 block w-full"
-	                    v-model="form.title"
-	                    required
-	                    autofocus
-	                    autocomplete="title"
-	                />
-
-	                <InputError class="mt-2" :message="form.errors.title" />
-	            </div>
-			</div>
-
-
-			<div class="flex-auto w-33 ">
-			   	<div class="p-2 m-2">
-	                <InputLabel for="company_name" value="Empresa" />
-
-	                <TextInput
-	                    id="company_name"
-	                    type="text"
-	                    class="mt-1 block w-full"
-	                    v-model="form.company_name"
-	                    required
-	                    autofocus
-	                    autocomplete="company_name"
-	                />
-
-	                <InputError class="mt-2" :message="form.errors.company_name" />
-	            </div>
-			</div>
-
-			<div class="flex-auto w-33 ">
-			   	<div class="p-2 m-2">
-	                <InputLabel for="company_city" value="Localidad" />
-
-	                <TextInput
-	                    id="company_city"
-	                    type="text"
-	                    class="mt-1 block w-full"
-	                    v-model="form.company_city"
-	                    required
-	                    autofocus
-	                    autocomplete="company_city"
-	                />
-
-	                <InputError class="mt-2" :message="form.errors.company_city" />
-	            </div>
-			</div>
-
-		</div>
-
-		<div class="flex">
-			<div class="flex-auto w-33">
-				<div class="flex">
-				   	<div class="p-2 m-2 flex-auto w-1/2">
-						<InputLabel for="date_start" value="Fecha Inicio" />
+	<div class="m-1">
+		<form @submit.prevent="submit">
+			<div class="flex">
+				<div class="flex-auto w-33 ">
+				   	<div class="p-1 m-1">
+		                <InputLabel for="title" value="Puesto" />
 
 		                <TextInput
-		                    id="date_start"
-		                    type="date"
+		                    id="title"
+		                    type="text"
 		                    class="mt-1 block w-full"
-		                    v-model="form.date_start"
+		                    v-model="form.title"
 		                    required
 		                    autofocus
-		                    autocomplete="date_start"
+		                    autocomplete="title"
 		                />
 
-		                <InputError class="mt-2" :message="form.errors.date_start" />
-				   	</div>
-
-				   	<div class="p-2 m-2 flex-auto w-1/2">
-						<InputLabel for="date_finish" value="Fecha Fin" />
-
-		                <TextInput
-		                    id="date_finish"
-		                    type="date"
-		                    class="mt-1 block w-full"
-		                    v-model="form.date_finish"
-		                    required
-		                    autofocus
-		                    autocomplete="date_finish"
-		                />
-
-		                <InputError class="mt-2" :message="form.errors.date_finish" />
-				   	</div>					
+		                <InputError class="mt-2" :message="form.errors.title" />
+		            </div>
 				</div>
 
-				<div class="flex w-full ">
-			   		<div class="flex-auto w-1/2" v-if="experience.id != 0">
-			   			<InputLabel for="id" value="id" />
+
+				<div class="flex-auto w-33 ">
+				   	<div class="p-1 m-1">
+		                <InputLabel for="company_name" value="Empresa" />
 
 		                <TextInput
-		                    id="id"
-		                    type="number"
+		                    id="company_name"
+		                    type="text"
 		                    class="mt-1 block w-full"
-		                    v-model="form.id"
-		                    autofocus
-		                    autocomplete="id"
-		                />
-			   		</div>
-			   		<div class="flex-auto w-1/2 invisible">
-			   			<InputLabel for="resume_id" value="id cv" />
-
-			   			<input 
-		                    id="resume_id"
-		                    type="number"
-		                    class="mt-1 block w-full"
-		                    v-model="form.resume_id"
+		                    v-model="form.company_name"
 		                    required
-		                    readonly 
+		                    autofocus
+		                    autocomplete="company_name"
 		                />
-			   		</div>
-			   	</div>
+
+		                <InputError class="mt-2" :message="form.errors.company_name" />
+		            </div>
+				</div>
+
+				<div class="flex-auto w-33 ">
+				   	<div class="p-1 m-1">
+		                <InputLabel for="company_city" value="Localidad" />
+
+		                <TextInput
+		                    id="company_city"
+		                    type="text"
+		                    class="mt-1 block w-full"
+		                    v-model="form.company_city"
+		                    required
+		                    autofocus
+		                    autocomplete="company_city"
+		                />
+
+		                <InputError class="mt-2" :message="form.errors.company_city" />
+		            </div>
+				</div>
 
 			</div>
 
+			<div class="flex">
+				<div class="flex-auto w-33">
+					<div class="flex">
+					   	<div class="p-1 m-1 flex-auto w-1/2">
+							<InputLabel for="date_start" value="Fecha Inicio" />
 
-			<div class="flex-auto w-66 ">
-			   	<div class="p-2 m-2">
-					<InputLabel for="job_description" value="Descripción" />
+			                <TextInput
+			                    id="date_start"
+			                    type="date"
+			                    class="mt-1 block w-full"
+			                    v-model="form.date_start"
+			                    required
+			                    autofocus
+			                    autocomplete="date_start"
+			                />
 
-	                <TextTextArea
-	                    id="job_description"
-	                    
-	                    class="mt-1 block w-full"
-	                    v-model="form.job_description"
-	                    required
-	                    autofocus
-	                    autocomplete="job_description"
-	                />
+			                <InputError class="mt-2" :message="form.errors.date_start" />
+					   	</div>
 
-	                <InputError class="mt-2" :message="form.errors.job_description" />
-			   	</div>
+					   	<div class="p-1 m-1 flex-auto w-1/2">
+							<InputLabel for="date_finish" value="Fecha Fin" />
 
-			   	<div>
-			   		<div class="flex items-center justify-start pt-3 m-2 row-start-4">
-                        <PrimaryButton v-if="props.experience.id == 0" class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                            Añadir al Currículum
-                        </PrimaryButton>
+			                <TextInput
+			                    id="date_finish"
+			                    type="date"
+			                    class="mt-1 block w-full"
+			                    v-model="form.date_finish"
+			                    required
+			                    autofocus
+			                    autocomplete="date_finish"
+			                />
 
-                        <div v-if="props.experience.id > 0" class="flex">
-                        	<SecondaryButton  class="ms-4" type="submit" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-	                            Actualizar
-	                        </SecondaryButton>
+			                <InputError class="mt-2" :message="form.errors.date_finish" />
+					   	</div>					
+					</div>
 
-	                        <div style="float: right;" >
-	                        	<button 
-							        type="button"
-							        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-							        @click="deleteExp"
-							    >
-							        Quitar
-							    </button>
-	                        </div>	                        
-                        </div>
-                        
-                    </div>
+					<div class="flex w-full ">
+				   		<div class="flex-auto w-1/2 invisible" v-if="experience.id != 0">
+				   			<InputLabel for="id" value="id" />
 
-                    <progress v-if="form.progress" :value="form.progress.percentage" max="100">
-                      {{ form.progress.percentage }}%
-                    </progress>
-			   	</div>
+			                <TextInput
+			                    id="id"
+			                    type="number"
+			                    class="mt-1 block w-full"
+			                    v-model="form.id"
+			                    autofocus
+			                    autocomplete="id"
+			                />
+				   		</div>
+				   		<div class="flex-auto w-1/2 invisible">
+				   			<InputLabel for="resume_id" value="id cv" />
+
+				   			<input 
+			                    id="resume_id"
+			                    type="number"
+			                    class="mt-1 block w-full"
+			                    v-model="form.resume_id"
+			                    required
+			                    readonly 
+			                />
+				   		</div>
+				   	</div>
+
+				</div>
+
+
+				<div class="flex-auto w-66 ">
+				   	<div class="p-1 m-1">
+						<InputLabel for="job_description" value="Descripción" />
+
+		                <TextTextArea
+		                    id="job_description"
+		                    
+		                    class="mt-1 block w-full"
+		                    v-model="form.job_description"
+		                    required
+		                    autofocus
+		                    autocomplete="job_description"
+		                />
+
+		                <InputError class="mt-2" :message="form.errors.job_description" />
+				   	</div>
+
+				   	<div>
+				   		<div class="flex items-center justify-start pt-3 m-2 row-start-4">
+	                        <PrimaryButton v-if="props.experience.id == 0" class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+	                            Añadir al Currículum
+	                        </PrimaryButton>
+
+	                        <div v-if="props.experience.id > 0" class="flex">
+	                        	<SecondaryButton  class="ms-4" type="submit" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+		                            Actualizar
+		                        </SecondaryButton>
+
+		                        <div style="float: right;" >
+		                        	<button 
+								        type="button"
+								        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+								        @click="deleteExp"
+								    >
+								        Quitar
+								    </button>
+		                        </div>	                        
+	                        </div>
+	                        
+	                    </div>
+
+	                    <progress v-if="form.progress" :value="form.progress.percentage" max="100">
+	                      {{ form.progress.percentage }}%
+	                    </progress>
+				   	</div>
+				</div>
 			</div>
-		</div>
 
-	</form>
+		</form>
+	</div>
 </template>
--- resources/js/Pages/CVFormEdit.vue
+++ resources/js/Pages/CVFormEdit.vue
@@ -4,14 +4,19 @@ import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
+import ChevronDown from '@/Components/ChevronDown.vue';
+import ChevronUp from '@/Components/ChevronUp.vue';
 import ExperienceElement from '@/Components/ExperienceElement.vue';
 import { Head, useForm } from '@inertiajs/vue3';
+import { ref } from "vue";
 
 let props = defineProps({
     cv: Object,
     experiences: Array,
+
 });
 
+const experienceVisible = ref(true);
 
 const form = useForm({
     title: props.cv.title || '',
@@ -21,6 +26,12 @@ const form = useForm({
 const submit = () => {
     form.put(route('updatecv'));
 };
+
+const toggleExperienceVisible = () => {
+    experienceVisible.value = !experienceVisible.value;
+}
+
+
 
 const addExperience = () => {
     const exp = {
@@ -61,15 +72,15 @@ const deleteExperience = (id) => {
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">                  
 
                    <div class="p-4 ">
-                        <div class="text-right">
+                        <div class="text-right" style="width: 25%;float: right;">
                             <a :href="route('view.cv')" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Ver Currículum</a>
                         </div>
                        <h1 class="text-2xl">Actualizar Currículum</h1>
 
-                       <div class="my-2">
+                       <div class="my-2 ">
                           <form @submit.prevent="submit">
                             <div class="flex">
-                                <div class="flex-auto w-64 ">
+                                <div class="flex-auto w-1/2 ">
                                     <div class="p-2 m-2">
                                         <InputLabel for="title" value="Título" />
 
@@ -87,7 +98,7 @@ const deleteExperience = (id) => {
                                     </div>                                    
                                 </div>
 
-                                <div class="flex-auto w-32">
+                                <div class="flex-auto w-1/4 ">
                                     <div class="invisible">
                                         <InputLabel for="cv_id" value="Id" />
 
@@ -103,13 +114,15 @@ const deleteExperience = (id) => {
                                         <InputError class="mt-2" :message="form.errors.cv_id" />
                                     </div>
                                 </div>
+
+                                <div class="flex-auto w-1/4 text-right">
+                                    <PrimaryButton class="mt-10" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                                        Guardar Título
+                                    </PrimaryButton>
+                                </div>
                             </div>
 
-                            <div class="flex items-center justify-start pt-3 m-2 row-start-4">
-                                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                                    Guardar Título
-                                </PrimaryButton>
-                            </div>
+                            
 
                             <progress v-if="form.progress" :value="form.progress.percentage" max="100">
                               {{ form.progress.percentage }}%
@@ -118,22 +131,37 @@ const deleteExperience = (id) => {
                           </form>
                        </div>
 
-                       <div class="mx-2 my-5">
+                       <div class="mx-2 my-5 rounded-md p-2 border border-gray-400 bg-gray-100">
+
+                            <div class="divide-b divide-dashed flex flex-row ">
+                                <div class="basis-1/4">
+                                    <h2>Experiencia</h2>
+                                </div>
+
+                                <div class="basis-3/4">
+                                    <button type="button" @click="toggleExperienceVisible" class="text-right w-full">
+                                        <ChevronUp v-if="experienceVisible"></ChevronUp>
+                                        <ChevronDown v-if="!experienceVisible"></ChevronDown>
+                                    </button>
+                                </div>
+                            </div>
                            
                            
-                           <h2>Experiencia</h2>
-                           <hr>
 
-                           <div class="my-3 py-2 bg-gray-200" v-for="(item, index) in experiences" :key="index + 1">
-                               <ExperienceElement 
-                                    :experience="item"
-                                    @experience-deleted="deleteExperience"
-                               />
+                           <div v-if="experienceVisible">
+                               <div class="my-3 py-2 bg-gray-200" v-for="(item, index) in experiences" :key="index + 1">
+                                   <ExperienceElement 
+                                        :experience="item"
+                                        @experience-deleted="deleteExperience"
+                                   />
+                               </div>
+
+                               <div class="text-right mb-4">
+                                   <button type="button" @click="addExperience()" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Añadir</button>
+                               </div>
                            </div>
 
-                           <div class="text-right mb-4">
-                               <button type="button" @click="addExperience()" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Añadir</button>
-                           </div>
+                           
                        </div>
                        
                    </div>
@@ -146,3 +174,9 @@ const deleteExperience = (id) => {
         </div>
     </AuthenticatedLayout>
 </template>
+
+<style>
+    .borde {
+        border: 1px solid black;
+    }
+</style>
